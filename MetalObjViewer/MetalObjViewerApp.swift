454b82e42eb64e734752276ea490611d3807eab3
C.H Lee <ximhear@gmail.com>
2025-09-01 00:57:34 +0900

    Initial Commit

---
+++ MetalObjViewer/MetalObjViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalObjViewerApp.swift
+//  MetalObjViewer
+//
+//  Created by gzonelee on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalObjViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
